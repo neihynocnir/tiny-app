d40936c2e93af97f0fea89454810d5dafd637fa7
Neihy Rincon <neihy.rincon@gmail.com>
2020-04-17 18:41:09 +0000

    adding test cases

---
+++ test/userByIDTest.js
@@ -0,0 +1,12 @@
+const { assert } = require('chai');
+const { userByID } = require('../controllers/userByID');
+const { users } = require('../db/usersDB');
+
+
+describe('userByID', function() {
+  it('should return a user for valid ID', function() {
+    const user = userByID('bgfaJ4')
+    const expectedOutput = users.bgfaJ4;
+    assert.equal(user, expectedOutput);
+  });
+});
